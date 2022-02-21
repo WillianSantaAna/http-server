2be474cdb9935cead1a948425f7328ff844060d3
Willian SantaAna <williansantaana@gmail.com>
2022-02-21 02:29:58 +0000

    finish first implementation

---
+++ src/server.rs
@@ -0,0 +1,52 @@
+use crate::http::{ParseError, Request, Response, StatusCode};
+use std::convert::TryFrom;
+use std::io::Read;
+use std::net::TcpListener;
+
+pub trait Handler {
+    fn handle_request(&mut self, request: &Request) -> Response;
+
+    fn handle_bad_request(&mut self, e: &ParseError) -> Response {
+        println!("Failed to parse request: {}", e);
+        Response::new(StatusCode::BadRequest, None)
+    }
+}
+
+pub struct Server {
+    addr: String,
+}
+
+impl Server {
+    pub fn new(addr: String) -> Server {
+        Server { addr }
+    }
+    pub fn run(self, mut handler: impl Handler) {
+        println!("Listening on {}", self.addr);
+
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Receive a request: {}", String::from_utf8_lossy(&buffer));
+
+                            let response = match Request::try_from(&buffer[..]) {
+                                Ok(request) => handler.handle_request(&request),
+                                Err(e) => handler.handle_bad_request(&e),
+                            };
+
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response: {}", e);
+                            }
+                        }
+                        Err(err) => println!("Failed to read from connection: {}", err),
+                    }
+                }
+                Err(err) => println!("Failed to establish a connection: {}", err),
+            }
+        }
+    }
+}
